fd0e02e3535471db5a71e2b6a8c36648a8bf50ad
Nickolaz <elanir358@gmail.com>
2020-04-26 11:01:55 +0300

    Delete .hello.mod.cmd

--- .hello.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/nick/programming/kernel/ldd_tutorial/hello.mod := { echo  /home/nick/programming/kernel/ldd_tutorial/hello.o; echo; } > /home/nick/programming/kernel/ldd_tutorial/hello.mod
