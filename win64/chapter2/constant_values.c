2427ff299ab257813a5cea1c63b0561334268110
Nickolazz1 <elanir358@gmail.com>
2020-11-09 17:52:02 +0200

    baytoeblya

---
+++ win64/chapter2/constant_values.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <limits.h>
+#include<float.h>
+/* определить диапазоны типов */
+int main()
+{
+	printf("Integer datatypes:\n");
+    printf("%d <= char <= %d\n", CHAR_MIN, CHAR_MAX);
+    printf("%d <= int <= %d\n", INT_MIN, INT_MAX);
+    printf("%ld <= long <= %ld\n", LONG_MIN, LONG_MAX);
+    printf("%d <= signed char <= %d\n", SCHAR_MIN, SCHAR_MAX);
+    printf("%d <= short <= %d\n", SHRT_MIN, SHRT_MAX);
+    printf("0 <= unsigned char <= %d\n", UCHAR_MAX);
+    printf("0 <= unsigned int <= %u\n", UINT_MAX);
+    printf("0 <= unsigned long <= %lu\n", ULONG_MAX);
+    printf("0 <= unsigned short <= %d\n", USHRT_MAX);
+    printf("\n");
+    printf("Float datatypes:\n");
+    printf("%e <= float <= %e\n", FLT_MIN, FLT_MAX);
+    printf("%e <= double <= %e\n", DBL_MIN, DBL_MAX);
+
+    return 0;
+}
