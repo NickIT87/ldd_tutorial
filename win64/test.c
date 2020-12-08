16e27f553eb07b872471bbd595694b7b7942c83e
Nickolazz1 <elanir358@gmail.com>
2020-12-08 22:50:43 +0200

    8 class task secondary school propaedeutics

--- win64/test.c
+++ win64/test.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
 
-int quantityOfHeat(double c, double m, double dt );
-
-int main() {
-	printf("%d\n", quantityOfHeat(880, 200, 23));
-
-
-	return 0;
+double quantityOfHeat(double c, double m, double dt)
+{	/*	The quantity of heat Q that changes the 
+ 	temperature ΔT of a mass m of a substance 
+ 	is given by Q = cmΔT	*/	
+	return c * m * dt;
 }
 
-/* The quantity of heat Q that changes the 
- temperature ΔT of a mass m of a substance 
- is given by Q = cmΔT */
-int quantityOfHeat(double c, double m, double dt )
+double substance(double Q, double m, double dt)
+{	/*	Joule per kilogram-degree Celsius	*/
+	return Q / (m * dt);
+}
+
+int phaseDt(int t1, int t2)
+{	/*	time per second	*/
+	return t2 - t1;
+}
+
+	/*	main flow programm	*/
+int main() 
 {
-	long double Q = c * m * dt;
-	return Q;
+	int m = 2000; 			/* Kg */
+	int t1 = 10; 			/* Celsius */
+	int t2 = 60;
+	double Q = 88000000; 	/* Joule */
+	/* ? */
+	double c = 0;
+	/* solve */
+	c = substance(Q, m, phaseDt(t1, t2));
+	printf("%f\n", c);
+	/* DEBUG */
+	printf("%f\n", quantityOfHeat(c, m, phaseDt(t1, t2)));
+
+	return 0;
 }
