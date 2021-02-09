8fce94b2ffbb747dd0ecca0bc0420e96fd96693b
salamanka <elanir358@gmail.com>
2021-02-09 18:15:45 +0200

    think about igraph c programming

--- linuxEnvTesting/lnxprj.c
+++ linuxEnvTesting/lnxprj.c
@@ -1,14 +1,17 @@
 #include <igraph.h>
 
-/* gcc lnxprj.c -I/usr/local/lib -L/usr/local/lib -ligraph -o igraph_teste */
+/* pkg-config --libs --cflags igraph */
+/* gcc lnxprj.c -I/usr/local/include/igraph -L/usr/local/lib -ligraph -o canonical_example */
 int main(void)
 {     
 	igraph_integer_t diameter;     
 	igraph_t graph;     
 	igraph_rng_seed(igraph_rng_default(), 42);
-	igraph_erdos_renyi_game(&graph, IGRAPH_ERDOS_RENYI_GNP,	1000, 5.0/1000, IGRAPH_UNDIRECTED, IGRAPH_NO_LOOPS);
+	igraph_erdos_renyi_game(&graph, IGRAPH_ERDOS_RENYI_GNP,	1000, 
+						5.0/1000, IGRAPH_UNDIRECTED, IGRAPH_NO_LOOPS);
 	igraph_diameter(&graph, &diameter, 0, 0, 0, IGRAPH_UNDIRECTED, 1);
-	printf("Diameter of a random graph with average degree 5: %d\n", (int) diameter);     
+	printf("Diameter of a random graph with average degree 5: %d\n", 
+						(int) diameter);     
 	igraph_destroy(&graph);     
 	return 0;
 }
--- linuxEnvTesting/mylib.c
+++ linuxEnvTesting/mylib.c
@@ -1,6 +1,34 @@
-#include <stdio.h>
+#include <igraph.h>
 
-void myfunc() 
+int main() 
 {
-	printf("void\n");
+	igraph_real_t avg_path;
+	igraph_t graph;
+	igraph_vector_t dimvector;
+	igraph_vector_t edges;
+	int i;
+	
+	igraph_vector_init(&dimvector, 2);
+	VECTOR(dimvector)[0]=30;
+	VECTOR(dimvector)[1]=30;
+	igraph_lattice(&graph, &dimvector, 0, IGRAPH_UNDIRECTED, 0, 1);
+	
+	igraph_rng_seed(igraph_rng_default(), 42);
+	igraph_vector_init(&edges, 20);
+	for (i=0; i<igraph_vector_size(&edges); i++) {
+		VECTOR(edges)[i] = rand() % (int)igraph_vcount(&graph);
+	}
+	
+	igraph_average_path_length(&graph, &avg_path, IGRAPH_UNDIRECTED, 1);
+	printf("Average path length (lattice):    %f\n", (double) avg_path);
+
+	igraph_add_edges(&graph, &edges, 0);
+	igraph_average_path_length(&graph, &avg_path, IGRAPH_UNDIRECTED, 1);
+	printf("Average path len(random lattice): %f\n", (double) avg_path);
+  
+	igraph_vector_destroy(&dimvector);
+	igraph_vector_destroy(&edges);
+	igraph_destroy(&graph);
+
+	return 0;
 }
