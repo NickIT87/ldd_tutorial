0349fa5763df164754c8e6a0726dbad1c82939fa
salamanka <elanir358@gmail.com>
2021-02-11 13:46:11 +0200

    first defenitions K&R 4.3

--- ctutkr/pollscalc.c
+++ ctutkr/pollscalc.c
@@ -4,9 +4,47 @@
 #define MAXOP 100       /* максимальный размер операнда */
 #define NUMBER '0'      /* сигнал, что обнаружено число */
 
+int getop(char s[]);
+void push(double);
+double pop(void);
+
+/* калькулятор с обратной польской записью */
 int main()
 {
-    
-
+    int type;
+    double op2;
+    char s[MAXOP];
+    while ((type = getop(s)) != EOF) 
+    {
+        switch (type) 
+        {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if(op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("error: zero divizor\n");
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+        }
+    }
     return 0;
 }
