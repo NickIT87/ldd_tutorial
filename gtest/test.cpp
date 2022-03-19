822f8bafefa5e6a8df39754c4b4169184c7e3e46
Nickolazz1 <elanir358@gmail.com>
2022-03-19 17:21:36 +0200

    gtest linux

---
+++ gtest/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+TEST(TestName, Subtest_1) {
+    ASSERT_FALSE(1 == 2);
+}
+
+TEST(TestName, Subtest_2) {
+    ASSERT_TRUE(1 == 2);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+// g++ test.cpp -lgtest -lgtest_main -pthread
+// https://www.youtube.com/watch?v=nbFXI9SDfbk&list=PL_dsdStdDXbo-zApdWB5XiF2aWpsqzV55
