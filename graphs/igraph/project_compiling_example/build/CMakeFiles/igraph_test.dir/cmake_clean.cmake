b3d1567f0accc30a7af15eee22b7346baa0918ae
NickIT87 <elanir358@gmail.com>
2023-08-18 22:28:36 +0300

    compiling project with cmake

---
+++ graphs/igraph/project_compiling_example/build/CMakeFiles/igraph_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/igraph_test.dir/igraph_test.c.o"
+  "CMakeFiles/igraph_test.dir/igraph_test.c.o.d"
+  "igraph_test"
+  "igraph_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/igraph_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
